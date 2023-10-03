f778fd1d6b4e539dd281c3410236bc0c46f69d23
hendrikparik <hendrikp@hendriks-MacBook-Air.local>
2023-10-03 19:58:54 +0300

    separating production initial

--- api/main.py
+++ api/main.py
@@ -48,7 +48,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://ec2-3-9-115-223.eu-west-2.compute.amazonaws.com/"
+    "http://ec2-3-9-115-223.eu-west-2.compute.amazonaws.com/",
     TEXT_TO_SPEECH_URL
 ]
 
