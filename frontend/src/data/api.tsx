c0b8143db41de5954ab8c05d0c5d6d61ff6dbe74
hendrikparik <hendrikp@hendriks-MacBook-Air.local>
2023-10-04 15:25:55 +0300

    added prod

--- frontend/src/data/api.tsx
+++ frontend/src/data/api.tsx
@@ -2,7 +2,7 @@
 
 
 export const apiUrl = process.env.NODE_ENV === 'production' ? 
-'http://ec2-3-9-115-223.eu-west-2.compute.amazonaws.com:3000' : 
+'http://rahvushaaling:3000' : 
 "http://localhost"
 
 // console.log(apiUrl);
